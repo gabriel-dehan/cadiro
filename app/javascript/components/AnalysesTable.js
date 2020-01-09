0b93baed87aa3da8ecb934ff1a0a8d384a0bc6f4
Gabriel Dehan <dehan.gabriel@gmail.com>
2020-01-09 23:10:29 +0100

    CSS refacto + Implemented full text search

--- app/javascript/components/AnalysesTable.js
+++ app/javascript/components/AnalysesTable.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { observer, inject } from 'mobx-react';
+import Fuse from 'fuse.js';
 
 import withStores from '../stores/withStores';
 import Analysis from './Analysis';
@@ -12,18 +13,90 @@ class AnalysesTable extends React.Component {
     data: PropTypes.object.isRequired,
   };
   
-  state = {
-    loaded: false,
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false,
+      analyses: this.props.data.analyses,
+    }
   }
 
   componentDidMount() {
     this.props.userStore.setUser(this.props.data.current_user);
     this.props.seasonsStore.setCurrent(this.props.data.current_season);
+    console.log(this.props.data.analyses)
+    this.fuse = new Fuse(this.props.data.analyses, {
+      keys: [
+        { name: 'item.name', weight: 0.7 }, 
+        { name: 'item.item_type', weight: 0.7 },
+        { name: 'item.rarity', weight: 0.3 },
+        { name: 'item.tags', weight: 0.5 },
+        { name: 'season_analyses.season.name', weight: 0.3 },
+        { name: 'season_analyses.buyout.currency', weight: 0.1 },
+        { name: 'season_analyses.sellout.currency', weight: 0.1 },
+        { name: 'season_analyses.comments', weight: 0.4 },
+      ],
+      tokenize: true,
+      shouldSort: true,
+      findAllMatches: true,
+      threshold: 0.4,
+      // includeMatches: true,
+      distance: 50,
+      maxPatternLength: 48,
+      minMatchCharLength: 2,
+    })
+
     this.setState({ loaded: true });
   }
 
+  search(value) {
+    if (value.length > 2) {
+      const result = this.fuse.search(value);
+      // console.log(result);
+      this.setState({ analyses: result });
+    } else {
+      this.setState({ analyses: this.props.data.analyses });
+    }
+  }
+
+  renderTable() {
+    const { analyses } = this.state;
+
+    if (analyses.length > 0) {
+      return (
+        <table className="table table-striped analyses-table">
+          <thead>
+            <tr>
+              <th className="watch" scope="col"></th>
+              <th colSpan="2" scope="col">Item</th>
+              <th scope="col">Search</th>
+              <th scope="col">B/O price</th>
+              <th scope="col">S/O price</th>
+              <th scope="col">~ Profit</th>
+              <th scope="col">ESD</th>
+              <th scope="col">Occurence</th>
+              <th scope="col">Trades</th>
+              <th scope="col">Tags</th>
+              <th scope="col">Comment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderAnalyses()}
+          </tbody>
+        </table>
+      );
+    } else {
+      return (
+        <div className="search-no-result">
+          Sorry, Cadiro didn't find any result for your query :'(
+        </div>
+      )
+    }
+  }
+
   renderAnalyses() {
-    const { analyses } = this.props.data;
+    const { analyses } = this.state;
     
     return analyses.map(analysis => {
         const season_analysis = analysis.season_analyses
@@ -46,27 +119,11 @@ class AnalysesTable extends React.Component {
     
     return (
       <div className="analyses-container">
-        <input type="text" className="analyses-search form-control" placeholder="Ring, Boots, Circle of Nostalgia..." />
-        <table className="table table-striped analyses-table">
-          <thead>
-            <tr>
-              <th className="watch" scope="col"></th>
-              <th colSpan="2" scope="col">Item</th>
-              <th scope="col">Search</th>
-              <th scope="col">B/O price</th>
-              <th scope="col">S/O price</th>
-              <th scope="col">~ Profit</th>
-              <th scope="col">ESD</th>
-              <th scope="col">Occurence</th>
-              <th scope="col">Trades</th>
-              <th scope="col">Tags</th>
-              <th scope="col">Comment</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderAnalyses()}
-          </tbody>
-        </table>
+        <div className="search-container">
+          <input onChange={(e) => { this.search(e.target.value) }} type="text" className="analyses-search form-control" placeholder="Ring, Boots, Circle of Nostalgia..." />
+        </div>
+        
+        {this.renderTable()}
       </div>
     );
   }
